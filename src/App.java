1779c3af625b8636914760a5b1e3c9bae6f14ac9
Victor Bruno <vitop.bruno@gmail.com>
2023-09-24 20:18:52 -0300

    questao 1

--- src/App.java
+++ src/App.java
@@ -1,9 +1,29 @@
+
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int myNumber = 88;
+            int X = scanner.nextInt(); // Valor a ser verificado
 
-        System.out.println(myNumber);
+            boolean primo = true;
+
+            if (X <= 1) {
+                primo = false;
+            } else {
+                for (int j = 2; j * j <= X; j++) {
+                    if (X % j == 0) {
+                        primo = false;
+                        break;
+                    }
+                }
+            }
+
+            if (primo) {
+                System.out.println(X + " é primo");
+            } else {
+                System.out.println(X + " não é primo");
+        }
     }
 }
