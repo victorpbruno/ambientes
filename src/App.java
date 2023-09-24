16af70bc20514f4efd45a25f87fe44f9e309a618
Victor Bruno <vitop.bruno@gmail.com>
2023-09-24 20:07:30 -0300

    .

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int myNumber = 88;
+
+        System.out.println(myNumber);
     }
 }
